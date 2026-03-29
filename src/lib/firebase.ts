5de34b3a308143a9ab4867b958cba71669c480ac
Jishaan Azmi <106030245+Jishaan-Azmi@users.noreply.github.com>
2026-03-29 18:54:31 +0530

    Update firebase.ts

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,12 +3,12 @@ import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCqSJ_ReLYeRjChAaTT6Jt5_M9OtD6MYiI",
-  authDomain: "mess-management-44381.firebaseapp.com",
-  projectId: "mess-management-44381",
-  storageBucket: "mess-management-44381.firebasestorage.app",
-  messagingSenderId: "700518943021",
-  appId: "1:700518943021:web:9ec98f8ceb7090a8faeba4"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 const app = initializeApp(firebaseConfig);
